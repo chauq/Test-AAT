2ddce9739bb7f11aa6b89743017c9e384538e425
Quang chau <quang.chau@durabledigital.com>
2018-01-04 12:16:12 +0000

    Updated packages and reworked the hooks file

--- Config/Hooks.cs
+++ Config/Hooks.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.Drawing;
 using BoDi;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -16,7 +17,7 @@ namespace Test_AAT.Config
         {
         }
 
-        public string GetSessionId()
+        public string getSessionID()
         {
             return base.SessionId.ToString();
         }
@@ -36,23 +37,31 @@ namespace Test_AAT.Config
         public const int PollingIntervalMilliseconds = 100;
         public IWebDriver _idriver;
 
-        CustomRemoteWebDriver _driver;
-        DesiredCapabilities capability = DesiredCapabilities.InternetExplorer();
-        readonly string isDesktopBrowser = ConfigurationManager.AppSettings.Get("IsDesktopBrowser");
-
-        ///// Run Locally - IE Driver
-        //[BeforeScenario]
-        //public void Initialise()
-        //{
-        //    _idriver = new InternetExplorerDriver();
-        //    _objectContainer.RegisterInstanceAs<IWebDriver>(_idriver);
-        //    _idriver.Navigate().GoToUrl(ConfigurationManager.AppSettings.Get("BaseUrl"));
-        //    _idriver.Manage().Window.Maximize();
-        //}
-
-        /// Run Locally - Chrome Driver
         [BeforeScenario]
         public void Initialise()
+        {
+            //InitialiseIEDriver();
+            InitialiseChromeDriver();
+            //InitialiseBrowserStack();
+        }
+
+        //[BeforeScenario("Mobile")]
+        //public void BeforeMoblieScenario()
+        //{
+        //    _idriver.Manage().Window.Size = new Size(450, 800);
+        //}
+
+        // Run Locally - IE Driver
+        private void InitialiseIEDriver()
+        {
+            _idriver = new InternetExplorerDriver();
+            _objectContainer.RegisterInstanceAs<IWebDriver>(_idriver);
+            _idriver.Navigate().GoToUrl(ConfigurationManager.AppSettings.Get("BaseUrl"));
+            _idriver.Manage().Window.Maximize();
+        }
+
+        // Run Locally - Chrome Driver
+        public void InitialiseChromeDriver()
         {
             _idriver = new ChromeDriver();
             _objectContainer.RegisterInstanceAs<IWebDriver>(_idriver);
@@ -60,56 +69,60 @@ namespace Test_AAT.Config
             _idriver.Manage().Window.Maximize();
         }
 
-        ///// Run on BrowserStack
-        //[BeforeScenario]
-        //public void Initialise()
-        //{
-        //    BrowserStackCapabilities();
+        // Run on BrowserStack
+        private void InitialiseBrowserStack()
+        {
+            BrowserStackCapabilities();
 
-        //    _driver = new CustomRemoteWebDriver(new Uri(ConfigurationManager.AppSettings.Get("BrowserStackUrl")), capability);
-        //    _objectContainer.RegisterInstanceAs<IWebDriver>(_driver);
-        //    _driver.Navigate().GoToUrl(ConfigurationManager.AppSettings.Get("BaseUrl"));
+            _driver = new CustomRemoteWebDriver(new Uri(ConfigurationManager.AppSettings.Get("BrowserStackUrl")), capability);
+            _objectContainer.RegisterInstanceAs<IWebDriver>(_driver);
+            _driver.Navigate().GoToUrl(ConfigurationManager.AppSettings.Get("BaseUrl"));
 
-        //    if (isDesktopBrowser == "true")
-        //        _driver.Manage().Window.Maximize();
+            if (isDesktopBrowser == "true")
+                _driver.Manage().Window.Maximize();
 
-        //    GetSessionId();
-        //}
+            GetSessionId();
+        }
 
-        //private void BrowserStackCapabilities()
-        //{
-        //    capability.SetCapability("browserstack.user", (ConfigurationManager.AppSettings.Get("BrowserStackUser")));
-        //    capability.SetCapability("browserstack.key", (ConfigurationManager.AppSettings.Get("BrowserStackKey")));
-        //    capability.SetCapability("browserstack.debug", (ConfigurationManager.AppSettings.Get("BrowserStackDebug")));
+        CustomRemoteWebDriver _driver;
+        DesiredCapabilities capability = DesiredCapabilities.InternetExplorer();
+        readonly string isDesktopBrowser = ConfigurationManager.AppSettings.Get("IsDesktopBrowser");
 
 
-        //    if (isDesktopBrowser == "true")
-        //    {
-        //        // Desktop Browser
-        //        capability.SetCapability("browser", (ConfigurationManager.AppSettings.Get("BrowserType")));
-        //        capability.SetCapability("browser_version", (ConfigurationManager.AppSettings.Get("BrowserVersion")));
-        //        capability.SetCapability("os", (ConfigurationManager.AppSettings.Get("OsType")));
-        //        capability.SetCapability("os_version", (ConfigurationManager.AppSettings.Get("OsVersion")));
-        //        capability.SetCapability("resolution", (ConfigurationManager.AppSettings.Get("OsResolution")));
-        //    }
-        //    else if (isDesktopBrowser == "false")
-        //    {
-        //        // Mobile Browser
-        //        capability.SetCapability("browserName", (ConfigurationManager.AppSettings.Get("BrowserName")));
-        //        capability.SetCapability("platform", (ConfigurationManager.AppSettings.Get("Platform")));
-        //        capability.SetCapability("device", (ConfigurationManager.AppSettings.Get("Device")));
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("*** Browser type not set in App Config");
-        //    }
-        //}
+        private void BrowserStackCapabilities()
+        {
+            capability.SetCapability("browserstack.user", (ConfigurationManager.AppSettings.Get("BrowserStackUser")));
+            capability.SetCapability("browserstack.key", (ConfigurationManager.AppSettings.Get("BrowserStackKey")));
+            capability.SetCapability("browserstack.debug", (ConfigurationManager.AppSettings.Get("BrowserStackDebug")));
 
-        //private void GetSessionId()
-        //{
-        //    string sessionId = _driver.getSessionID();
-        //    Console.WriteLine("BROWSERSTACK SESSION ID: " + sessionId);
-        //}
+
+            if (isDesktopBrowser == "true")
+            {
+                // Desktop Browser
+                capability.SetCapability("browser", (ConfigurationManager.AppSettings.Get("BrowserType")));
+                capability.SetCapability("browser_version", (ConfigurationManager.AppSettings.Get("BrowserVersion")));
+                capability.SetCapability("os", (ConfigurationManager.AppSettings.Get("OsType")));
+                capability.SetCapability("os_version", (ConfigurationManager.AppSettings.Get("OsVersion")));
+                capability.SetCapability("resolution", (ConfigurationManager.AppSettings.Get("OsResolution")));
+            }
+            else if (isDesktopBrowser == "false")
+            {
+                // Mobile Browser
+                capability.SetCapability("browserName", (ConfigurationManager.AppSettings.Get("BrowserName")));
+                capability.SetCapability("platform", (ConfigurationManager.AppSettings.Get("Platform")));
+                capability.SetCapability("device", (ConfigurationManager.AppSettings.Get("Device")));
+            }
+            else
+            {
+                Console.WriteLine("*** Browser type not set in App Config");
+            }
+        }
+
+        private void GetSessionId()
+        {
+            string sessionId = _driver.getSessionID();
+            Console.WriteLine("BROWSERSTACK SESSION ID: " + sessionId);
+        }
 
         [AfterScenario]
         public void CleanUp()
